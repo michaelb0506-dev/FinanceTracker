f9bfa1206790c30b606bbe31dac081cf8a75d7e7
Michael Bennion <3018354b@student.gla.ac.uk>
2026-07-01 16:10:18 +0100

    registered models to admin site

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from rango.models import Account, Transaction
+from rango.models import Account, Transaction, Category
 
 admin.site.register(Account)
 admin.site.register(Transaction)    
+admin.site.register(Category)
