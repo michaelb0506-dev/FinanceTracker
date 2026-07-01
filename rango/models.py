69e47af295fcd1c7a803a2dd6e7649fcb2c45f7d
Michael Bennion <3018354b@student.gla.ac.uk>
2026-07-01 15:34:19 +0100

    Added Category and ammended models

--- rango/models.py
+++ rango/models.py
@@ -2,16 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Account(models.Model):
-    AccountName = models.CharField(max_length=100)
+    account_name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
-    userId = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.AccountName
+        return self.account_name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
 
 class Transaction(models.Model):
-    ammount = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.CharField(max_length=100)
-    dictate = models.DateTimeField(auto_now_add=True)
-    type = models.BooleanField(default=True)  
-    accountID = models.ForeignKey(Account, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    date = models.DateField()
+    is_income = models.BooleanField(default=True)  
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.amount} - {self.category} - {self.date}"
