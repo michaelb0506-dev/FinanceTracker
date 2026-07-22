df6b51518aca19ada79d1dacdf543bb432e3c4a6
Michael Bennion <3018354b@student.gla.ac.uk>
2026-07-22 19:55:39 +0100

    Add login page and user authentication flow

--- rango/urls.py
+++ rango/urls.py
@@ -5,4 +5,5 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
 from django.http import HttpResponse
 
+@login_required
 def home(request):
     return render(request, 'rango/home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username = username, password = password)
+        if user:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, 'rango/login.html', {"error": "Invalid Username or Password"})
+
+    
+    else:
+        return render(request, 'rango/login.html')
+          
+       
+
