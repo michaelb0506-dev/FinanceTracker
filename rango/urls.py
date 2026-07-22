3b5067236594a10f297d4b97bf8dc00ec6fb13f4
Michael Bennion <3018354b@student.gla.ac.uk>
2026-07-22 17:59:09 +0100

    Created templates file and homepage url,view and template

--- rango/urls.py
+++ rango/urls.py
@@ -4,5 +4,5 @@ from rango import views
 app_name = 'rango'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the rango index.")
+def home(request):
+    return render(request, 'rango/home.html')
