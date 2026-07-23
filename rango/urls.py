3bd071a06f1850f62c51a97984346ee9e8d9cbf2
Michael Bennion <3018354b@student.gla.ac.uk>
2026-07-23 20:54:23 +0100

    Added logout button to homepage

--- rango/urls.py
+++ rango/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from rango import views
+from django.contrib.auth import views as auth_views
 
 app_name = 'rango'
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout')
 ]
